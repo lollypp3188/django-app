16e3c5de3928eedd6b0b77532614a622906f4503
lollypp3188 <annayakymovaaa@gmail.com>
2024-09-10 17:49:55 +0300

    feat: add utils with paginator and search

---
+++ app/users/signals.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import (
+  post_save,
+  post_delete,
+)
+from django.dispatch import receiver
+from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+# @receiver(post_save, sender=Profile)
+def create_profile(sender, instance, created, **kwargs):
+  print('Profile signal triggered.')
+  if created:
+    user = instance
+    profile = Profile.objects.create(
+      user=user,
+      username=user.username,
+      email=user.email,
+      name=user.first_name,
+    )
+    subject = 'Welcome to DevSearch'
+    message = 'We are glad you are here.'
+    send_mail(
+      subject,
+      message,
+      settings.EMAIL_HOST_USER,
+      [profile.email],
+      fail_silently=False,
+    )
+
+
+def delete_user(sender, instance, **kwargs):
+  user = instance.profile
+  user.delete()
+  print('Deleting user...')
+
+
+def update_user(sender, instance, created, **kwargs):
+  profile = instance
+  user = profile.user
+  if created == False:
+    user.first_name = profile.name
+    user.username = profile.username
+    user.email = profile.email
+    user.save()
+
+
+
+post_save.connect(create_profile, sender=User)
+post_save.connect(update_user, sender=Profile)
+post_delete.connect(delete_user, sender=Profile)
