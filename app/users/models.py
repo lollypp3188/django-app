75bbb5bf0d34e15571a4a8ef2266ed87aa3ff173
lollypp3188 <annayakymovaaa@gmail.com>
2024-09-17 17:50:32 +0300

    fix: linting

--- app/users/models.py
+++ app/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
-
 class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, null=True, blank=True
@@ -14,8 +13,8 @@ class Profile(models.Model):
     short_intro = models.CharField(max_length=200, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     profile_image = models.ImageField(
-    null=True, blank=True, upload_to='profiles/', 
-    default='profiles/user-default.png'
+        null=True, blank=True, upload_to='profiles/', 
+        default='profiles/user-default.png'
     )
     social_github = models.CharField(max_length=200, null=True, blank=True)
     social_twitter = models.CharField(max_length=200, null=True, blank=True)
@@ -30,7 +29,6 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.username)
 
-
 class Skill(models.Model):
     owner = models.ForeignKey(
         Profile, on_delete=models.CASCADE, null=True, blank=True
@@ -44,14 +42,13 @@ class Skill(models.Model):
     def __str__(self):
         return str(self.name)
 
-
 class Message(models.Model):
     sender = models.ForeignKey(
         Profile, on_delete=models.SET_NULL, null=True, blank=True
     )
     recipient = models.ForeignKey(
-      Profile, on_delete=models.SET_NULL, null=True, blank=True,
-      related_name='messages'
+        Profile, on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='messages'
     )
     name = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=200, null=True, blank=True)
