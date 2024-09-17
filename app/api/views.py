ec106ebdc7910a1c61660eb6b3787013de31031a
lollypp3188 <annayakymovaaa@gmail.com>
2024-09-17 16:29:14 +0300

    fix: syntax

--- app/api/views.py
+++ app/api/views.py
@@ -1,61 +1,63 @@
 from django.http import JsonResponse
 from rest_framework.decorators import (
-  api_view,
-  permissions_classes,
+    api_view,
+    permission_classes,
 )
 from rest_framework.permissions import (
-  IsAuthenticated,
-  IsAdminUser,
+    IsAuthenticated,
+    IsAdminUser,
 )
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from projects.models import (
-  Project,
-  Review,
+    Project,
+    Review,
 )
 from api import serializers
 
 @api_view(['GET'])
 def get_routes(request):
-  routes = [
-    {'GET': '/api/projects'},
-    {'GET': '/api/projects/id'},
-    {'POST': '/api/projects/id/vote'},
+    routes = [
+        {'GET': '/api/projects'},
+        {'GET': '/api/projects/id'},
+        {'POST': '/api/projects/id/vote'},
 
-    {'POST': '/api/users/token'},
-    {'POST': '/api/users/token/refresh'},
-  ]
-  return Response(routes)
+        {'POST': '/api/users/token'},
+        {'POST': '/api/users/token/refresh'},
+    ]
+    return Response(routes)
 
-
-
-# @permissions_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 @api_view(['GET'])
 def get_projects(request):
-  projects = Project.objects.all()
-  serializer = ProjectSerializer(projects, many=True)
-  return Response(serializer.data)
-
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def get_project(request, pk):
-  project = Project.objects.all(id=pk)
-  serializer = ProjectSerializer(project, many=False)
-  return Response(serializer.data)
-
+    try:
+        project = Project.objects.get(id=pk)
+        serializer = ProjectSerializer(project, many=False)
+        return Response(serializer.data)
+    except Project.DoesNotExist:
+        return Response({'error': 'Project not found'}, status=404)
 
 @api_view(['POST'])
-@permissions_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated])
 def project_vote(request, pk):
-  project = Project.objects.get(id=pk)
-  user = request.user.profile
-  data = request.data
-  review, created =  Review.objects.get_or_create(
-    owner=user,
-    project=project,
-  )
-  review.value = data['value']
-  review.save()
-  project.get_vote_count
-  serializer = ProjectSerializer(project, many=False)
-  return Response(serializer.data)
+    try:
+        project = Project.objects.get(id=pk)
+        user = request.user.profile
+        data = request.data
+        review, created = Review.objects.get_or_create(
+            owner=user,
+            project=project,
+        )
+        review.value = data.get('value')
+        review.save()
+        project.get_vote_count()
+        serializer = ProjectSerializer(project, many=False)
+        return Response(serializer.data)
+    except Project.DoesNotExist:
+        return Response({'error': 'Project not found'}, status=404)
